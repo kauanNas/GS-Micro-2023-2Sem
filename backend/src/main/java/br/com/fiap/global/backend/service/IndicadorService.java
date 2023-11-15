6c2ac635589460633b4b32e553ab9e43822fbba3
kauanNas <kauan802@gmail.com>
2023-11-15 01:45:33 -0300

    criada classe service para indicador

---
+++ backend/src/main/java/br/com/fiap/global/backend/service/IndicadorService.java
@@ -0,0 +1,19 @@
+package br.com.fiap.global.backend.service;
+
+import br.com.fiap.global.backend.model.Indicador;
+import br.com.fiap.global.backend.dto.IndicadorDto;
+import br.com.fiap.global.backend.repository.IndicadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndicadorService {
+
+    @Autowired
+    private IndicadorRepository repository;
+
+    public IndicadorDto buscaIndicador(Long id) {
+        Indicador indicador = repository.getReferenceById(id);
+        return new IndicadorDto(indicador.getAno(), indicador.getConsumo());
+    }
+}
