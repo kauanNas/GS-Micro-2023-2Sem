9d46aef649655bf8ad03145ef727ba68297bf6e2
kauanNas <kauan802@gmail.com>
2023-11-23 18:49:23 -0300

    adicionado mais testes de carga

--- test/scripts.js
+++ test/scripts.js
@@ -2,11 +2,17 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 10,
-    duration: '30s',
+    vus: 100,
+    duration: '300s',
 };
 
 export default function(){
-    http.get('http://localhost/indicadores/1');
+    http.get('http://localhost:8080/indicador/1');
+    sleep(1);
+    http.get('http://localhost:8080/indicadores');
+    sleep(1);
+    http.get('http://localhost:8080/buscaMetricas/3.1.1');
+    sleep(1);
+    http.get('http://localhost:8080/objetivos');
     sleep(1);
 }
